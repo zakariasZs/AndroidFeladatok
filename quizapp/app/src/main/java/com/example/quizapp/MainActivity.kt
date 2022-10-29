357e8aaf232ff045afc280609cd8a703af0cdaea
zakariasZs <91305855+zakariasZs@users.noreply.github.com>
2022-10-29 16:27:58 +0300

    2022.10.29
    -select contact

--- quizapp/app/src/main/java/com/example/quizapp/MainActivity.kt
+++ quizapp/app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.quizapp
 
 import android.content.Intent
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.util.Log
 import android.view.View
 import android.widget.Button
@@ -13,6 +16,7 @@ import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private val TAG: String = javaClass.simpleName
+    private val PICK_CONTACT_REQUEST_CODE = 101
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,18 +42,45 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+//        val chooseButton : Button = findViewById(R.id.button2)
+//        chooseButton.setOnClickListener{
+//            Snackbar.make(chooseButton, "Text label", Snackbar.LENGTH_LONG)
+//                .setAction("Action") {
+//                    val toast = Toast.makeText(applicationContext, "Action pressed", Toast.LENGTH_SHORT)
+//                    toast.show()
+//                }
+//                .show()
+//        }
+
         val chooseButton : Button = findViewById(R.id.button2)
         chooseButton.setOnClickListener{
-            Snackbar.make(chooseButton, "Text label", Snackbar.LENGTH_LONG)
-                .setAction("Action") {
-                    val toast = Toast.makeText(applicationContext, "Action pressed", Toast.LENGTH_SHORT)
-                    toast.show()
-                }
-                .show()
+            pickContact()
         }
+    }
 
+    private fun pickContact() {
+        val pickContact = Intent(Intent.ACTION_PICK)
+        pickContact.type = ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE
+        startActivityForResult(pickContact, PICK_CONTACT_REQUEST_CODE)
+    }
 
-
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_CONTACT_REQUEST_CODE && data != null) {
+            val contactData: Uri? = data.data
+            if (contactData != null) {
+                val cursor: Cursor? = contentResolver.query(contactData, null, null, null, null)
+                if (cursor != null && cursor.moveToFirst()) {
+                    val nameIndex: Int =
+                        cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)
+                    val name: String = cursor.getString(nameIndex)
+                    cursor.close()
+                    val intent = Intent(this, SecondActivity::class.java)
+                    intent.putExtra("user_name", name)
+                    startActivity(intent)
+                }
+            }
+        }
     }
 
     override fun onStart() {
--- quizapp/app/src/main/java/com/example/quizapp/fragment/QuizStartFragment.kt
+++ quizapp/app/src/main/java/com/example/quizapp/fragment/QuizStartFragment.kt
@@ -1,8 +1,24 @@
 package com.example.quizapp.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.quizapp.R
 
 class QuizStartFragment : Fragment(R.layout.quiz_start) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+    override fun onCreateView (
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.quiz_start, container, false)
+    }
+
 
 }
