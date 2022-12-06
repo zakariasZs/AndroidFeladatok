48e32e70b7925f5a08d068279066baa290986a25
zakariasZs <91305855+zakariasZs@users.noreply.github.com>
2022-12-06 13:47:48 +0200

    DatePicker for addTask fragment

--- Tracker/app/src/main/java/com/example/tracker/fragment/AddTaskFragment.kt
+++ Tracker/app/src/main/java/com/example/tracker/fragment/AddTaskFragment.kt
@@ -1,9 +1,12 @@
 package com.example.tracker.fragment
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
 import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -11,10 +14,16 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tracker.R
 import com.example.tracker.databinding.AddTaskScreenBinding
+import java.sql.Timestamp
+import java.text.SimpleDateFormat
+import java.time.Year
+import java.util.*
 
-class AddTaskFragment : Fragment(R.layout.add_task_screen) {
+class AddTaskFragment : Fragment(R.layout.add_task_screen), DatePickerDialog.OnDateSetListener {
 
     private lateinit var binding: AddTaskScreenBinding
+    private val calendar = Calendar.getInstance()
+    private val formatter = SimpleDateFormat("yyyy.MM.dd", Locale.ROOT)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,12 +42,30 @@ class AddTaskFragment : Fragment(R.layout.add_task_screen) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
+        binding.deadLinePicker.setOnClickListener{
+            DatePickerDialog( view.context,
+                this,
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
 
 //        binding.addQuestion.setOnClickListener { view ->
 //            this.findNavController().navigate(R.id.questionAddFragment)
 //        }
 
+    }
+
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        Log.e("XXX- Calendar: ", "$year -- $month -- $dayOfMonth")
+        calendar.set(year, month, dayOfMonth)
+        displayFormatedDate(calendar.timeInMillis)
+    }
+
+    private fun displayFormatedDate( timestamp: Long){
+        binding.deadLinePicker.text = formatter.format(timestamp)
     }
 
 }
